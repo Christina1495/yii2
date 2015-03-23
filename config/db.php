e55b75c65e46425532dc089d357deaf1645c7a72
Christina1495 <Rikki0188@rambler.ru>
2015-03-23 23:00:06 +0300

    --

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=Books',
-    'username' => 'Rikki',
-    'password' => '198721',
-    'charset' => 'utf8',
-];
